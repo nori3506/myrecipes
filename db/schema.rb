aae9ec52439ac8f5a6979dc6c50a189ad4cc076d
nori3506 <nori19900809@gmail.com>
2018-12-02 07:50:27 +0000

    add one to many association between chef and recipe

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_01_183056) do
+ActiveRecord::Schema.define(version: 2018_12_02_070345) do
 
   create_table "chefs", force: :cascade do |t|
     t.string "chefname"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 2018_12_01_183056) do
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "chef_id"
   end
 
 end
--- test/models/recipe_test.rb
+++ test/models/recipe_test.rb
@@ -3,7 +3,13 @@ require 'test_helper'
 class RecipeTest < ActiveSupport::TestCase
   
   def setup
-    @recipe = Recipe.new(name: "vegetable", description: "great vegetable recipe")
+    @chef = Chef.create(chefname: "nori", email: "nori@email.com")
+    @recipe = @chef.recipes.build(name: "vegetable", description: "great vegetable recipe")
+  end
+  
+  test "recipe without chef should be invalid" do
+    @recipe.chef_id = nil
+    assert_not @recipe.valid?
   end
   
   test "recipe should be valid" do
@@ -29,4 +35,6 @@ class RecipeTest < ActiveSupport::TestCase
     @recipe.description= "a" * 501
     assert_not @recipe.valid?
   end
+  
+  
 end  
