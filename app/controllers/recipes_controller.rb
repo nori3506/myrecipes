6a5f40fe9516e9d23aa39b1619c0b4ec82abe9b9
nori3506 <nori19900809@gmail.com>
2018-12-03 17:59:44 +0000

    Refactor and clean out layout

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,54 +1,56 @@
 class RecipesController < ApplicationController
-    
-    def index 
-      @recipes = Recipe.all
+  before_action :set_recipe, only: [:show, :edit, :update]
+  
+  def index 
+    @recipes = Recipe.all
+  end
+  
+  def show
+  end
+  
+  def new
+    @recipe = Recipe.new
+  
+  end
+  
+  def create 
+    @recipe = Recipe.new(recipe_params)
+    @recipe.chef = Chef.first
+    if @recipe.save
+      flash[:success] = "Recipe was created successfully!"
+      redirect_to recipe_path(@recipe)
+    else
+      render 'new'
     end
-    
-    def show
-      @recipe=Recipe.find(params[:id])
-    end
-    
-    def new
-      @recipe = Recipe.new
-    
-    end
-    
-    def create 
-      @recipe = Recipe.new(recipe_params)
-      @recipe.chef = Chef.first
-      if @recipe.save
-        flash[:success] = "Recipe was created successfully!"
-        redirect_to recipe_path(@recipe)
-      else
-        render 'new'
-      end
-    end
-    
-    def edit
-      @recipe = Recipe.find(params[:id])
-      
-    end
+  end
+  
+  def edit
 
-    def update
-      @recipe = Recipe.find(params[:id])
-      if @recipe.update(recipe_params)
-        flash[:success]="Recipe was updated successfully"
-        redirect_to recipe_path(@recipe)
-      else
-        render 'edit'
-      end
+  end
+
+  def update
+    if @recipe.update(recipe_params)
+      flash[:success]="Recipe was updated successfully"
+      redirect_to recipe_path(@recipe)
+    else
+      render 'edit'
     end
-    
-    def destroy
-      Recipe.find(params[:id]).destroy
-      flash[:success]="Recipe was successfully deleted"
-      redirect_to recipes_path
-    end
+  end
+  
+  def destroy
+    Recipe.find(params[:id]).destroy
+    flash[:success]="Recipe was successfully deleted"
+    redirect_to recipes_path
+  end
 
 private
 
-    def recipe_params
-      params.require(:recipe).permit(:name, :description)
-    end
+  def set_recipe
+    @recipe = Recipe.find(params[:id])
+  end
+
+  def recipe_params
+    params.require(:recipe).permit(:name, :description)
+  end
 
 end
