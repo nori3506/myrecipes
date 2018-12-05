b0057acabc31d9b36c8fe583ef7b0b4659ffcb11
nori3506 <nori19900809@gmail.com>
2018-12-05 06:07:38 +0000

    edit action with test

---
+++ test/integration/chefs_edit_test.rb
@@ -0,0 +1,29 @@
+require 'test_helper'
+
+class ChefsEditTest < ActionDispatch::IntegrationTest
+  
+  def setup
+  @chef = Chef.create!(chefname: "nori", email:"nori@gmail.com",
+                         password: "password", password_confirmation: "password")
+  end
+  
+  test "reject an invalid edit" do
+    get edit_chef_path(@chef)
+    assert_template 'chefs/edit'
+    patch chef_path(@chef), params: {chef: {chefname:" ", email:"nori@email.com "}}
+    assert_template 'chefs/edit'
+    assert_select 'h2.panel-title'
+    assert_select 'div.panel-body'
+  end
+  
+  test "accept valid signup" do
+    get edit_chef_path(@chef)
+    assert_template 'chefs/edit'
+    patch chef_path(@chef), params: {chef: {chefname:"Noria", email:"noria@email.com"}}
+    assert_redirected_to @chef
+    assert_not flash.empty?
+    @chef.reload
+    assert_match "Noria", @chef.chefname
+    assert_match "noria@email.com", @chef.email
+  end
+end
