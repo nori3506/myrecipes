d7169e8d12eaefd04fb101675162484c9e80ad7a
nori3506 <nori19900809@gmail.com>
2018-12-06 11:58:52 +0000

    Add chefs destroy and tests

--- app/controllers/chefs_controller.rb
+++ app/controllers/chefs_controller.rb
@@ -35,7 +35,13 @@ class ChefsController < ApplicationController
     else
       render 'edit'
     end
-
+  end
+  
+  def destroy
+    @chef = Chef.find(params[:id])
+    @chef.destroy
+    flash[:danger] = "Chef and all associated recipes have been deleted"
+    redirect_to chefs_path
   end
   
 private
