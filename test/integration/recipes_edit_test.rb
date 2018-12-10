a8867786ebb7ac42e9fc3b90f2cef2ec3dc83929
nori3506 <nori19900809@gmail.com>
2018-12-10 05:06:29 +0000

    Complete authentication systemwith test

--- test/integration/recipes_edit_test.rb
+++ test/integration/recipes_edit_test.rb
@@ -9,6 +9,7 @@ class RecipesEditTest < ActionDispatch::IntegrationTest
   end
   
   test 'reject invalid recipe update' do
+    sign_in_as(@chef, "password")
     get edit_recipe_path(@recipe)
     assert_template 'recipes/edit'
     patch recipe_path(@recipe),params: {recipe: {name: "",description:"some comment"}}
@@ -18,6 +19,7 @@ class RecipesEditTest < ActionDispatch::IntegrationTest
   end
   
   test 'successfully edit a recipe' do
+    sign_in_as(@chef, "password")
     get edit_recipe_path(@recipe)
     assert_template 'recipes/edit'
     updated_name ="Pork chicken beef special"
