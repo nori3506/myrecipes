2fde085946c0b994e6586d32454a7821062e05e2
nori3506 <nori19900809@gmail.com>
2018-12-11 16:56:18 +0000

    complete ingredients resource

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -52,7 +52,7 @@ private
   end
 
   def recipe_params
-    params.require(:recipe).permit(:name, :description)
+    params.require(:recipe).permit(:name, :description, ingredient_ids: [])
   end
   
   def require_same_user
